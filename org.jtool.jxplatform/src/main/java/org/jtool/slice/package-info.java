689b8650c0f54c958e2205475d02eb5745e064fa
Katsuhisa Maruyama <maru@cs.ritsumei.ac.jp>
2025-06-29 00:01:40 +0900

    Remove module file

--- org.jtool.jxplatform/src/main/java/org/jtool/slice/package-info.java
+++
@@ -1,9 +0,0 @@
-/*
- *  Copyright 2022
- *  Software Science and Technology Lab., Ritsumeikan University
- */
-
-/**
- * This package provides APIs that extract a slice and its corresponding source code. 
- */
-package org.jtool.slice;
