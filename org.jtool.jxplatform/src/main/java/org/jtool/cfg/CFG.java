be883f0dfc2b78cb09fbc06550042bb3bb2007c7
Katsuhisa Maruyama <maru@cs.ritsumei.ac.jp>
2026-08-29 12:19:10 +0900

    Remove unneeded SuppressWarnings

--- org.jtool.jxplatform/src/main/java/org/jtool/cfg/CFG.java
+++ org.jtool.jxplatform/src/main/java/org/jtool/cfg/CFG.java
@@ -420,7 +420,6 @@ public class CFG extends Graph<CFGNode, ControlFlow> {
      * @param fallthroughOk {@code true} if fall-through edges can be traversed, otherwise {@code false}
      * @return the collection of the forward-traversed nodes
      */
-    @SuppressWarnings("unused")
     public List<CFGNode> forwardReachableNodes(CFGNode startnode, boolean loopbackOk, boolean fallthroughOk) {
         return forwardReachableNodes(startnode, loopbackOk, fallthroughOk, node -> { return false; });
     }
@@ -485,7 +484,6 @@ public class CFG extends Graph<CFGNode, ControlFlow> {
      * @param fallthroughOk {@code true} if fall-through edges can be traversed, otherwise {@code false}
      * @return the collection of the backward-traversed nodes
      */
-    @SuppressWarnings("unused")
     public List<CFGNode> backwardReachableNodes(CFGNode startnode, boolean loopbackOk, boolean fallthroughOk) {
         return backwardReachableNodes(startnode, loopbackOk, fallthroughOk, node -> { return false; });
     }
